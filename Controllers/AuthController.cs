bcbe071a662961ff8ba78dd0739ff50c14f74304
Shahriar Shamim <159273209+shahriarshamim@users.noreply.github.com>
2024-12-24 14:11:38 +0600

    Login page

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ost_Inventory_b4.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
