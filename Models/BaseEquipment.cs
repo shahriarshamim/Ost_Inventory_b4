8ef8c93e1b5fd8300b46556870669fbb9a2f972a
Shahriar Shamim <159273209+shahriarshamim@users.noreply.github.com>
2025-01-11 23:20:12 +0600

    update

---
+++ Models/BaseEquipment.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Ost_Inventory_b4.Models
+{
+    public class BaseEquipment
+    {
+        public int EquipmentId { get; set; }
+        public string EquipmentName { get; set; }
+        public int Quantity { get; set; }
+        public int Stock { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime ReceiveDate { get; set; }
+
+        public List<BaseEquipment> LstEquipment()
+        {
+            List<BaseEquipment> lstEquips = new List<BaseEquipment>();
+
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOST_LstEquipment";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.CommandTimeout = 0;
+
+                //cmd.ExecuteNonQuery();//insert delete update 
+                SqlDataReader reader = cmd.ExecuteReader(); //fetch mode list data 
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        BaseEquipment baseEquipment = new BaseEquipment();
+                        baseEquipment.EquipmentId = Convert.ToInt32(reader["EquipmentId"].ToString());
+                        baseEquipment.EquipmentName = reader["EquipmentName"].ToString();
+                        baseEquipment.Quantity = Convert.ToInt32(reader["Quantity"].ToString());
+                        baseEquipment.Stock = Convert.ToInt32(reader["Stock"].ToString());
+                        baseEquipment.EntryDate = Convert.ToDateTime(reader["EntryDate"].ToString());
+                        baseEquipment.ReceiveDate = Convert.ToDateTime(reader["ReceiveDate"].ToString());
+                        lstEquips.Add(baseEquipment);
+                    }
+                }
+
+                //transaction
+                reader.Close();
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+            return lstEquips;
+        }
+    }
+}
